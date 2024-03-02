4e16007324d3acdf41b7243b71e3a93682248e43
Marushka Fernandes <marusfern@gmail.com>
2024-03-02 11:51:50 +0530

    newcommit

--- Gemini_chat/End-To-End-Gemini-Project/main.py
+++ Gemini_chat/End-To-End-Gemini-Project/main.py
@@ -1,46 +1,3 @@
-'''from dotenv import load_dotenv
-load_dotenv() ## loading all the environment variables
-
-import streamlit as st
-import os
-import google.generativeai as genai
-
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-## function to load Gemini Pro model and get repsonses
-model=genai.GenerativeModel("gemini-pro") 
-chat = model.start_chat(history=[])
-def get_gemini_response(question):
-    
-    response=chat.send_message(question,stream=True)
-    return response
-
-##initialize our streamlit app
-
-st.set_page_config(page_title="Q&A Demo")
-
-st.header("Gemini LLM Application")
-
-# Initialize session state for chat history if it doesn't exist
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history'] = []
-
-input=st.text_input("Input: ",key="input")
-submit=st.button("Ask the question")
-
-if submit and input:
-    response=get_gemini_response(input)
-    # Add user query and response to session state chat history
-    st.session_state['chat_history'].append(("You", input))
-    st.subheader("The Response is")
-    for chunk in response:
-        st.write(chunk.text)
-        st.session_state['chat_history'].append(("Bot", chunk.text))
-st.subheader("The Chat History is")
-    
-for role, text in st.session_state['chat_history']:
-    st.write(f"{role}: {text}")'''
-
 from dotenv import load_dotenv
 load_dotenv()  # Loading all the environment variables
 
@@ -63,24 +20,24 @@ def get_gemini_response(question):
 # Function to get grain and farming storage facilities
 def get_grain_and_storage_facilities(location):
     # Use the location parameter to query Google Generative AI about grain and farming storage facilities
-    question = f"Grain and farming storage facilities near {location} kilometre radius of me. Please return as json response the name, phone number and address"
+    question = f"Grain and farming storage facilities near {location} kilometre radius of me. Please return the name, phone number and address"
     return get_gemini_response(question)
 
 # Function to get input suppliers of seeds and farming products
 def get_input_suppliers(location):
     # Use the location parameter to query Google Generative AI about input suppliers
-    question = f"Input suppliers of seeds and farming products near {location} kilometre radius of me. Please return as json response the name, phone number and address"
+    question = f"Input suppliers of seeds and farming products near {location} kilometre radius of me. Please return the name, phone number and address"
     return get_gemini_response(question)
 
 # Function to get soil testing services
 def get_soil_testing_services(location):
     # Use the location parameter to query Google Generative AI about soil testing services
-    question = f"Soil testing services near {location} kilometre radius of me. Please return as json response the name, phone number and address"
+    question = f"Soil testing services near {location} kilometre radius of me. Please return the name, phone number and address"
     return get_gemini_response(question)
 
 # Initialize streamlit app
-st.set_page_config(page_title="Q&A Demo")
-st.header("Gemini LLM Application")
+st.set_page_config(page_title="Services provider")
+st.header("Enter location radius within which you want to access services")
 
 # Initialize session state for chat history if it doesn't exist
 if 'chat_history' not in st.session_state:
@@ -88,7 +45,7 @@ if 'chat_history' not in st.session_state:
 
 # Get user input
 input_location = st.text_input("Location: ", key="location")
-submit = st.button("Ask the question")
+submit = st.button("Submit")
 
 if submit and input_location:
     # Get responses based on user queries
@@ -122,10 +79,3 @@ if submit and input_location:
 st.subheader("The Chat History is")
 for role, text in st.session_state['chat_history']:
     st.write(f"{role}: {text}")
-
-    
-
-
-
-    
-
